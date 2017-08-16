c07e73a98b6ffea60b73067a6215a5b10870c9d0
tuddman <scott.tudd@gmail.com>
2017-08-16 18:17:46 -0400

    takes CLI options when uberjar'ed

--- src/clojiota/core.clj
+++ src/clojiota/core.clj
@@ -2,10 +2,11 @@
   (:import (com.iota.iri IRI))
   (:require 
     [clojure.tools.cli :refer  [parse-opts]]
-    [taoensso.timbre :as log]))
+    [taoensso.timbre :as log])
+  (:gen-class))
 
 
 (defn -main
-  []
+  [& args]
   (log/info "IOTA is starting up.")
-  (IRI/main (into-array ["-p" "14265"])))
+  (IRI/main (into-array args)))
