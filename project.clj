0cdcc67dbbc27bf36996e92d7fce052484babba4
tuddman <scott.tudd@gmail.com>
2017-09-27 03:14:25 +0000

    updates IRI to latest: 1.4.0

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clojiota "0.1.1-SNAPSHOT"
+(defproject clojiota "0.1.2-SNAPSHOT"
   :description "A Clojure library built on top of the IOTA Reference Implementation. Adds useful enhancements."
   :url "https://github.com/tuddman/clojiota"
   :license {:name "MIT License"
             :url "https://github.com/tuddman/clojiota/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/tools.cli "0.3.5"]
                  [clj.qrgen "0.4.0"]
-                 [com.iota/iri "1.3.2.2"]
+                 [com.iota/iri "1.4.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [crypto-random "1.2.0"]]
   
